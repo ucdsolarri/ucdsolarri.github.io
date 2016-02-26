96c4e7e91a341c97ef773a088b36c349d6e32a4d
andydlin <andydlin@gmail.com>
2016-02-26 14:26:00 -0800

    new changes

--- js/site.js
+++ js/site.js
@@ -1,1 +1,30 @@
 var elem = new Foundation.Tabs($('#progressTabs'));
+
+$('.scroll-link').on('click', function(e) {
+    e.preventDefault();
+
+    var href = $(this).attr('href');
+    var pos = $(href).offset().top;
+    $('body, html').animate({
+        scrollTop: pos,
+    },1000);
+});
+
+$(window).scroll(function() {
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop >= 100) {
+        $('.navbar').addClass('active');
+    } else {
+        $('.navbar').removeClass('active');
+    }
+
+    $('.on-scroll').each(function() {
+        var posTop = $(this).offset().top;
+        var diff = posTop - scrollTop;
+        if(diff <= 600) {
+            $(this).addClass('in-view');
+            var width = $(this).find('.current-progress').attr('progress');
+            $(this).find('.current-progress').css('width', width);
+        }
+    });
+});
